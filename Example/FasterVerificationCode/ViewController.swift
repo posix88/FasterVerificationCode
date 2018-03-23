0c409f3cc09b9e5446f1431fe44cc2236877abd3
Antonino Francesco Musolino <amusolino@deloitte.it>
2018-03-23 11:21:55 +0100

    Initial commit

---
+++ Example/FasterVerificationCode/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FasterVerificationCode
+//
+//  Created by Posix88 on 03/23/2018.
+//  Copyright (c) 2018 Posix88. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
