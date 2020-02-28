bc39e73fc5d6f910961b27d431656c3db949539b
Antonino Francesco Musolino <ninomusolino@gmail.com>
2020-02-28 09:28:46 +0100

    cleared project folder from pod project

--- Example/Pods/Target Support Files/FasterVerificationCode/FasterVerificationCode-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FasterVerificationCode : NSObject
-@end
-@implementation PodsDummy_FasterVerificationCode
-@end
--- Example/Pods/Target Support Files/Pods-FasterVerificationCode_Tests/Pods-FasterVerificationCode_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FasterVerificationCode_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FasterVerificationCode_Tests
-@end
